5acd88f097b1aa1a945932610bb2dc26435aea90
Anastasiazzzz <stce.fr@gmail.com>
2024-04-08 21:31:23 +0500

    переделала негативный тест и исправила тесты ng

--- L4/src/test/java/org/example/SortTestNegative.java
+++ L4/src/test/java/org/example/SortTestNegative.java
@@ -6,92 +6,27 @@ import org.testng.annotations.Test;
 public class SortTestNegative {
 
     @Test(groups = "negative")
-    void testInsertionSort() {
-        SortInt sort = new SortInt();
-        int Arr[] = {1, 15, 2, 61, 8, 70};
-
-        sort.insertionSort(Arr);
-
-        int expectedArr[] = {1, 8, 15, 2, 61, 70};
-        Assert.assertEquals(expectedArr, Arr);
-    }
-
-    @Test(groups = "negative")
-    void testCombSort() {
-        SortInt sort = new SortInt();
-        int Arr[] = {1, 15, 2, 61, 8, 70};
-
-        sort.combSort(Arr);
-
-        int expectedArr[] = {1, 8, 15, 2, 61, 70};
-        Assert.assertEquals(expectedArr, Arr);
-    }
-
-    @Test(groups = "negative")
-    void testSelectionSort() {
-        SortInt sort = new SortInt();
-        int Arr[] = {1, 15, 2, 61, 8, 70};
-
-        sort.selectionSort(Arr);
-
-        int expectedArr[] = {1, 8, 15, 2, 61, 70};
-        Assert.assertEquals(expectedArr, Arr);
-    }
-
-    @Test(groups = "negative")
     void testQuickHoareSort() {
         SortInt sort = new SortInt();
 
         int Arr[] = {1, 15, 2, 61, 8, 70};
 
-        sort.quickHoareSort(Arr);
+        sort.hoareSort(Arr, Arr.length, Arr.length - 1);
 
-        int expectedArr[] = {1, 8, 15, 2, 61, 70};
-        Assert.assertEquals(expectedArr, Arr);
-    }
-
-    @Test(groups = "negative")
-    void testInsertionSortDouble() {
-        SortDouble sort = new SortDouble();
-        double Arr[] = {1.15, 15.8, 2, 61.9, 8.71, 70.5};
-
-        sort.insertionSortDouble(Arr);
-
-        double expectedArr[] = {1.15, 8.71, 2, 61.9, 15.8, 70.5};
-        Assert.assertEquals(expectedArr, Arr);
-    }
-
-    @Test(groups = "negative")
-    void testCombSortDouble() {
-        SortDouble sort = new SortDouble();
-        double Arr[] = {1.15, 15.8, 2, 61.9, 8.71, 70.5};
-
-        sort.combSortDouble(Arr);
-
-        double expectedArr[] = {1.15, 8.71, 2, 61.9, 15.8, 70.5};
-        Assert.assertEquals(expectedArr, Arr);
-    }
-
-    @Test(groups = "negative")
-    void testSelectionSortDouble() {
-        SortDouble sort = new SortDouble();
-        double Arr[] = {1.15, 15.8, 2, 61.9, 8.71, 70.5};
-
-        sort.selectionSortDouble(Arr);
-
-        double expectedArr[] = {1.15, 8.71, 2, 61.9, 15.8, 70.5};
+        int expectedArr[] = {1, 15, 2, 61, 8, 70};
         Assert.assertEquals(expectedArr, Arr);
     }
 
     @Test(groups = "negative")
     void testQuickHoareSortDouble() {
+
         SortDouble sort = new SortDouble();
 
         double Arr[] = {1.15, 15.8, 2, 61.9, 8.71, 70.5};
 
-        sort.quickHoareSortDouble(Arr);
+        sort.hoareSortDouble(Arr, Arr.length, Arr.length - 1);
 
-        double expectedArr[] = {1.15, 8.71, 2, 61.9, 15.8, 70.5};
+        double expectedArr[] = {1.15, 15.8, 2, 61.9, 8.71, 70.5};
         Assert.assertEquals(expectedArr, Arr);
     }
 }
